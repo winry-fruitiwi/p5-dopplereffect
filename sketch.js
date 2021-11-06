f69583167b1ef63c0898fbcf6d37a310d788e6e5
Winry <winry.tian@gmail.com>
2021-11-06 14:05:19 -0700

    add coding plan

--- sketch.js
+++ sketch.js
@@ -2,6 +2,17 @@
 @author Winry
 @date 2021-11-06
 
+code plan:
+    create SoundWave class
+        constructor(x, y) {this.pos, this.lifetime, this.r}
+        update, show, isExpired
+    create Speaker class
+        constructor(x, y) {this.pos, this.vel, this.acc, this.lifetime, this.r}
+        update, applyForce, show, emit (speakers emit sound waves)
+    add rainbow colors
+    maybe add some sort of cool addition, like calculating what frequency of
+    sounds a point gets
+
  */
 let font
 
@@ -15,7 +26,5 @@ function setup() {
 }
 
 function draw() {
-    background(209, 80, 30)
-    
     background(234, 34, 24)
 }
