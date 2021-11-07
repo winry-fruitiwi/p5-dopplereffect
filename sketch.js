d0d435c16787f05e1ed4cec4d0e68f893bcafa5c
Winry <winry.tian@gmail.com>
2021-11-07 13:42:03 -0800

    add speaker class, emit sound waves, make speaker move

--- sketch.js
+++ sketch.js
@@ -15,6 +15,7 @@ code plan:
 
  */
 let font
+let tigrex // a Tigrex has a really loud roar!
 let tigrexRoar // nothing is louder than a Tigrex's roar!
 
 function preload() {
@@ -24,13 +25,22 @@ function preload() {
 function setup() {
     createCanvas(640, 360)
     colorMode(HSB, 360, 100, 100, 100)
+    rectMode(CENTER)
     tigrexRoar = new SoundWave(width / 2, height / 2)
+    tigrex = new Speaker(width/2, height/2)
 }
 
 function draw() {
+    translate(width/2, height/2)
     background(234, 34, 24)
-    tigrexRoar.show()
-    tigrexRoar.update()
+    // tigrexRoar.show()
+    // tigrexRoar.update()
+
+    tigrex.show()
+    // tigrex.applyForce(new p5.Vector(0, 0.1))
+    tigrex.pos = new p5.Vector(100*cos(frameCount/30), 100*sin(frameCount/30))
+    tigrex.update()
+    tigrex.emit()
 }
 
 class SoundWave {
@@ -52,7 +62,7 @@ class SoundWave {
 
     update() {
         // controls the decrement for the lifetime and increment for the radius
-        const DECREMENT = 2
+        const DECREMENT = 1
 
         this.lifetime -= DECREMENT
         this.radius += DECREMENT
@@ -61,5 +71,51 @@ class SoundWave {
     // has the wave passed away yet?
     isExpired() {
         return this.lifetime <= 0
+    }
+}
+
+class Speaker {
+    constructor(x, y) {
+        this.pos = new p5.Vector(x, y)
+        this.vel = new p5.Vector()
+        this.acc = new p5.Vector()
+
+        // keeps track of the number of pulses this speaker has emitted
+        this.soundWaves = []
+    }
+
+    show() {
+        fill(0, 0, 100, 60)
+        noStroke()
+        square(this.pos.x, this.pos.y, 16)
+
+        for (let wave of this.soundWaves) {
+            wave.show()
+        }
+    }
+
+    update() {
+        this.pos.add(this.vel)
+        this.vel.add(this.acc)
+        this.acc.mult(0) // equivalent to this.acc = new p5.Vector
+
+        for (let i = 0; i < this.soundWaves.length; i++) {
+            let wave = this.soundWaves[i]
+            wave.update()
+            if (wave.isExpired()) {
+                this.soundWaves.slice(i, i)
+            }
+        }
+    }
+
+    applyForce(force) {
+        // F=ma, but m = 1 so F=a
+        this.acc.add(force)
+    }
+
+    emit() {
+        if (frameCount % 2 === 0) {
+            this.soundWaves.push(new SoundWave(this.pos.x, this.pos.y))
+        }
     }
 }
